be9148ce50d06f7c01e917b2b2114e1aa90d345c
Hiago Henrique Gomes <hiago_h3@hotmail.com>
2022-01-12 15:15:28 -0300

    Organização

--- Exerc/script/scrpit.js
+++ Exerc/script/scrpit.js
@@ -10,20 +10,20 @@ function carregar( ) {
     window.setInterval('carregar()', 1000)
     if (hora>=0 && hora<6) {
         //boa madrugada
-        img.src = 'madrugada.jpg'
+        img.src = 'imagens/madrugada.jpg'
         document.body.style.background = '#0d0e12'
 
     }else if(hora>=6 && hora <12){
         //bom dia
-        img.src = 'manhã.jpg'
+        img.src = 'imagens/manhã.jpg'
         document.body.style.background = '#7d92b1'
     }else if (hora>=12 && hora <18){
         //boa tarde
-        img.src = 'tarde.jpg'
+        img.src = 'imagens/tarde.jpg'
         document.body.style.background = '#ff6002'
     }else{
         //boa noite
-        img.src = 'noite.jpg'
+        img.src = 'imagens/noite.jpg'
         document.body.style.background = '#000000'
     }
     
